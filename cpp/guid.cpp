f4e12b8b7fa933ff093b23ae5e21e39fe888fa30
asialiugf <asialiugf@sina.com>
2018-02-28 23:09:59 +0800

    .

---
+++ cpp/guid.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <uuid/uuid.h>  
+  
+int main()  
+{  
+    uuid_t uu;  
+    int i;  
+    uuid_generate( uu );  
+  
+    for(i=0;i<16;i++)  
+    {  
+        printf("%02X-",uu[i]);  
+    }  
+    printf("\n");  
+    return 0;  
+}
