b938382b7903dee0efe014cf5615171c10a26da0
asialiugf <asialiugf@sina.com>
2018-06-10 20:22:46 +0800

    .

---
+++ cpp/co.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+    printf("\033[31mThis is RED.\n\033[0m");
+    std::cout << "\033[31mThis is RED.\n\033[0m";
+    return 0;
+}
